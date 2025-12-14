4639011eee1642c74db36a00e809466b914ed8b9
Jacob Hafström <jacobhafstrom25@MacBook-Air-som-tillhor-Jacob.local>
2025-12-14 19:16:06 +0100

    fix: use full raw company list from raw-companies-list.ts

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,21 +1,4 @@
 
-import { COMPANIES_PART_1 } from './companies-part1';
-import { COMPANIES_PART_2 } from './companies-part2';
-import { COMPANIES_PART_3 } from './companies-part3';
-import { COMPANIES_PART_4 } from './companies-part4';
-import { COMPANIES_PART_5 } from './companies-part5';
-import { COMPANIES_PART_6 } from './companies-part6';
-import { COMPANIES_PART_7 } from './companies-part7';
-import { COMPANIES_PART_8 } from './companies-part8';
-import { COMPANIES_PART_9 } from './companies-part9';
+import { RAW_CSV_DATA } from './raw-companies-list';
 
-export const ALL_COMPANIES_RAW =
-    COMPANIES_PART_1 + '\n' +
-    COMPANIES_PART_2 + '\n' +
-    COMPANIES_PART_3 + '\n' +
-    COMPANIES_PART_4 + '\n' +
-    COMPANIES_PART_5 + '\n' +
-    COMPANIES_PART_6 + '\n' +
-    COMPANIES_PART_7 + '\n' +
-    COMPANIES_PART_8 + '\n' +
-    COMPANIES_PART_9;
+export const ALL_COMPANIES_RAW = RAW_CSV_DATA;
